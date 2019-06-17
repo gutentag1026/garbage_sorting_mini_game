102617f076b9096d1c23c9bf4e65fb74db6565bf
Wendy Huang <huanghuang5087@hotmail.com>
2019-06-17 13:06:57 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+let img;
+let img_path = ['https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fcharger.jpg?v=1560585865908','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2FComputer_Circuit_Board.jpg?v=1560575086848','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fcan.jpg?v=1560586359564','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fenvelope.jpg?v=1560586359938','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2FGlassware.jpg?v=1560586359985','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fkitchen.jpg?v=1560586360118','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fleather_shoes.jpg?v=1560586360675','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fbattery.jpg?v=1560599074597','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2FTrumps-XRay--131918.jpg?v=1560599085905','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fpesticide.jpg?v=1560599093212','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Flight.jpg?v=1560599134674','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fthermometer.jpg?v=1560599134886','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fmedicine.png?v=1560599135639','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fnail-oil.jpg?v=1560599135849','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fdog-food.jpg?v=1560602232208','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2FChinese_medicine.jpg?v=1560602232276','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Frice.webp?v=1560602232521','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fnuts.jpg?v=1560602232755','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fshrimp.jpg?v=1560602233163','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fflower.jpg?v=1560602234424','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fbeans.jpg?v=1560602236064','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fcigarette.jpg?v=1560603211590','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fgalsses.jpg?v=1560611272137','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fdiaper.jpg?v=1560603208177','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fhair.jpg?v=1560603207408','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2FPlastic-Bag.jpg?v=1560603207368','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Fham_bone.jpg?v=1560603207223','https://cdn.glitch.com/0b2f6689-f9d2-487d-8bb3-10b55da7c416%2Faid.jpg?v=1560603207044'];
+let rubbish = ['power charger','circuit board', 'can','envelope','glassware','kitchen board','leather shoes', 'battery', 'x-ray','pesticide', 'energy-saving light','thermometer', 'expired medicine', 'nail oil', 'dog food', 'Chinese medicine waste', 'rice', 'nuts', 'shrimp', 'flower', 'bean', 'cigarette', 'glass', 'diaper', 'hair', 'plastic', 'big bone', 'band-aid'];
+let current_img = 0;
+let score = 0;
+let w = 70;
+let h = 50;
+function setup() {
+  createCanvas(8.5 * w, 10 * h);
+  c = color(255, 10, 10);
+  d = color(120, 220, 20);
+  e = color(246, 250, 27);
+  g = new garbage(80,380,w, h);
+  img = [];
+  for(let i = 0; i < 28; i++)
+    img.push(loadImage(img_path[i]));  
+  
+}
+let y = 0;
+function draw() {
+  if (current_img == 28)
+  {
+  background(0,255,0);
+  fill(color(255,255,255));
+  textSize(30);
+  text('you got ' + score + ' points out of 28 points',100,280);
+    return ;
+  } 
+  drawBand(c, d, e, 0, 10);
+  g.display();
+ 
+ 
+  if (y < 300 && mouseX != 0)
+  { 
+    image(img[current_img],mouseX-35,y,60,60);
+     y = y + 3;
+  }
+  if (y == 300 && mouseX >= 450 && mouseX <= 550 && current_img >= 0 && current_img <= 6)
+    score++;
+  if (y == 300 && mouseX >= 330 && mouseX < 450 && current_img >= 7 && current_img <= 13)
+    score++;
+  if (y == 300 && mouseX >= 210 && mouseX < 350 && current_img >= 14 && current_img <= 20)
+    score++;
+  if (y == 300 && mouseX >= 90 && mouseX < 200 && current_img >= 21 && current_img <= 27)
+    score++;
+  fill(color(0,0,0,100));
+  rect(450,30,130,50);
+  fill(color(255,255,255));
+  textSize(30);
+  text(score,460,65);
+  fill(color(0,0,0,100));
+  rect(450,100,130,50);
+  fill(color(255,255,255));
+  textSize(15);
+ text(rubbish[current_img],460,130);
+  if (y >= 300)
+  {
+    y = 0;
+    current_img = (current_img + 1);
+  }
+}
+
+function drawBand(x, y, z, ypos, barWidth) {
+  let num = 3;
+  let colorOrder = [x, y, z];
+  for (let i = 0; i < width; i += barWidth * num) {
+    for (let j = 0; j < num; j++) {
+      fill(colorOrder[j]);
+      rect(i + j * barWidth, ypos, barWidth, 500);
+    }
+  }
+}
+class garbage
+{
+  constructor(e_xpos,e_ypos,e_w,e_h) 
+  {
+    this.expos = e_xpos;
+    this.eypos = e_ypos;
+    this.e_width = e_w;
+    this.e_height = e_h;
+    this.distance = 30;
+    this.rect_xpos = this.expos - this.e_width/2;
+    this.rect_ypos = this.eypos;
+    this.rect_width = this.e_width;
+    this.rect_height = this.e_width * 1.5;
+    this.classification = ['dry', 'wet', 'hazardous', 'recycable'];
+
+  }
+     
+  display()
+  {
+ let d = 0;
+    for(let i = 0; i < 4; i++)
+    {
+      d += 50;
+      fill(color(255,0,0,200));
+      rect(this.rect_xpos + this.e_width * i + d, this.rect_ypos, this.rect_width, this.rect_height, 0,0,20,20);
+      fill(color(255,255,0,100));
+      ellipse(this.expos + this.e_width * i + d, this.eypos, this.e_width, this.e_height);
+      textSize(12);
+      fill(color(255,255,200));
+      text(this.classification[i], this.rect_xpos+this.rect_width * i + d + 5,this.rect_ypos+ this.e_width * 1.2);
+      
+    }
+
+  }
+}
